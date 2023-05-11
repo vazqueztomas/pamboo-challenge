9c3b19f23f9b94c2addc96b3b30fe284787385db
Tomas Vazquez <vazquezt2018@gmail.com>
2023-05-11 00:33:32 -0300

    try to fix cors problems

--- api/config/allowedOrigins.js
+++ api/config/allowedOrigins.js
@@ -4,6 +4,7 @@ const allowedOrigins = [
   "127.0.0.1:5173",
   "http://127.0.0.1:5173",
   "http://localhost:5173",
+  "https://pamboo-challenge.vercel.app/",
 ];
 
 module.exports = allowedOrigins;
