ddba0628778da870300776d428c60bf8da202840
Tomas Vazquez <vazquezt2018@gmail.com>
2023-05-10 22:50:36 -0300

    delete: unused libraries, and components

--- client/components/TrashIcon.jsx
+++
@@ -1,7 +0,0 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrashAlt } from "@fortawesome/free-regular-svg-icons";
-const TrashIcon = () => {
-  return <FontAwesomeIcon icon={faTrashAlt} />;
-};
-
-export default TrashIcon;
